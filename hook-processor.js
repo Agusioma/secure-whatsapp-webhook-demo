875998971afcdfd77c5cfc6e6b1397194c4d6c85
Agusioma <terryterrence200@gmail.com>
2023-01-20 13:42:34 +0300

    major progress

--- hook-processor.js
+++ hook-processor.js
@@ -56,6 +56,7 @@ app.prepare()
 
         server.listen(3000, (err) => {
             if (err) throw err
+            console.log("hey")
             console.log('> Ready on http://localhost:3000')
           })
     })
